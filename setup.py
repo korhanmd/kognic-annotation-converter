0a8c18f07d1fbb61380a69fd232b0b4e85d9d20e
Korhan Mutludoğan <korhanmd@gmail.com>
2023-06-18 22:38:16 +0200

    Add setup flie to build package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='annotation_converter',
+    version='1.0.0',
+    author='Korhan Mutludogan',
+    author_email='korhanmd@gmail.com',
+    description='Annotation converter from Kognic format to Open Label format',
+    packages=find_packages(),
+    install_requires=[
+        "flask"
+    ],
+)
